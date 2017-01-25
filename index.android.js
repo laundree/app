7473a0ab26830b5c7a25046c9f87114b789cd6d8
Malene Sjørslev Søholm <malene.soeholm@gmail.com>
2017-01-25 21:53:48 +0100

    feat: Create QR scanner
    Initial QR scanner added that opens link in browser

--- index.android.js
+++ index.android.js
@@ -3,51 +3,81 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-
+'use strict';
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  StyleSheet,
-  Text,
-  View
+    Alert,
+    AppRegistry,
+    Dimensions,
+    Linking,
+    StyleSheet,
+    Text,
+    TouchableHighlight,
+    View
 } from 'react-native';
+import Camera from 'react-native-camera';
 
-export default class LaundreeApp extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
-      </View>
+class LaundreeApp extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Camera
+                    ref={(cam) => {
+                        this.camera = cam;
+                    }}
+                    style={styles.preview}
+                    aspect={Camera.constants.Aspect.fill}
+                    onBarCodeRead={onBarCodeRead}>
+                    <Text style={styles.capture} onPress={this.takePicture.bind(this)}>[CAPTURE]</Text>
+                </Camera>
+            </View>
+        );
+    }
+
+    takePicture() {
+        this.camera.capture()
+            .then((data) => console.log(data))
+            .catch(err => console.error(err));
+    }
+}
+
+function onBarCodeRead(e) {
+    console.log(
+        "Barcode found",
+        "Data: " + e.data
     );
-  }
+    Alert.alert('Laundree invitation found!',
+        'Do you want to open the Laundree in your browser?',
+        [
+            {text: 'OK', onPress: () => {
+                console.log('OK pressed');
+                Linking.openURL(e.data).catch(err => console.error('An error occurred', err));
+            }},
+            {text: 'Cancel', onPress: () => console.log('Cancel pressed'), style: 'cancel'}
+        ]);
+
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+    container: {
+        flex: 1
+    },
+    preview: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+        height: Dimensions.get('window').height,
+        width: Dimensions.get('window').width
+    },
+    capture: {
+        flex: 0,
+        backgroundColor: '#fff',
+        borderRadius: 5,
+        color: '#000',
+        padding: 10,
+        margin: 40
+    }
 });
 
 AppRegistry.registerComponent('LaundreeApp', () => LaundreeApp);
