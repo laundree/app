2a24fb5ca591310eff8b93182520699bd366a4f8
Malene Sjørslev Søholm <malene.soeholm@gmail.com>
2017-02-05 17:01:02 +0100

    Add Timetable and Login files
    Add files and switch in MainRouter

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -4,9 +4,16 @@
 
 import React from 'react';
 import QrCodeScanner from './QrCodeScanner'
+import Login from './Login'
+import Timetable from './Timetable'
 
 export default class MainRouter extends React.Component {
     render () {
-        return <QrCodeScanner/>
+        switch (Math.floor(Math.random() * 3)) {
+            case 0: return <QrCodeScanner/>
+            case 1: return <Login/>
+            case 2: return <Timetable/>
+        }
+        return null;
     }
 }
