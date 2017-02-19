4e581042d121fa227af302439899eb040acba2c7
Christian Budde Christensen <budde377@gmail.com>
2017-02-19 18:49:35 +0100

    feat: Restructuring and update project
    * Move resources
    * Update project
    * Enable IOS
    * Enable Lint

--- index.android.js
+++ index.android.js
@@ -3,11 +3,11 @@
  * https://github.com/facebook/react-native
  * @flow
  */
-'use strict';
-import React from 'react';
-import {AppRegistry} from 'react-native';
-import MainRouter from './src/MainRouter'
+'use strict'
+import React from 'react'
+import {AppRegistry} from 'react-native'
+import MainRouter from './src/react/views/MainRouter'
 
 const LaundreeApp = () => <MainRouter/>
 
-AppRegistry.registerComponent('LaundreeApp', () => LaundreeApp);
+AppRegistry.registerComponent('LaundreeApp', () => LaundreeApp)
