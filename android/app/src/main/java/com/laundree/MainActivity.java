d3e4f99f1e1d548639539ad4f2c8a2beb7ec40ac
Christian Budde Christensen <budde377@gmail.com>
2017-02-27 19:14:45 +0100

    feat: Upgrade react-native, add icons, change name

--- android/app/src/main/java/com/laundree/MainActivity.java
+++ android/app/src/main/java/com/laundree/MainActivity.java
@@ -1,4 +1,4 @@
-package com.laundreeapp;
+package com.laundree;
 
 import com.facebook.react.ReactActivity;
 
@@ -10,6 +10,6 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "LaundreeApp";
+        return "Laundree";
     }
 }
