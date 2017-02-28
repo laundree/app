414be33948eca6bda5419bfbbf228b7efd64bb5c
Christian Budde Christensen <budde377@gmail.com>
2017-02-28 12:45:15 +0100

    feat: Add google app deployment

---
+++ tools/release.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+. secrets/android/release.conf
+
+NPM_VERSION=$(node -p -e "require('./package.json').version")
+export VERSION_CODE=$(git rev-list --count HEAD)
+export VERSION_NAME=${TRAVIS_TAG:-$NPM_VERSION}
+echo "RELEASING ANDROID VERSION: $VERSION_NAME VERSION CODE: $VERSION_CODE"
+
+cp secrets/android/my-release-key.keystore android/app/my-release-key.keystore
+cp secrets/android/service-account-credentials.json android/app/service-account-credentials.json
+
+cd android && ./gradlew assembleRelease && ./gradlew publishApkRelease
