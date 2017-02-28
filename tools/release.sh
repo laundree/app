98e5275f2f0ded3a60242bf9bb33981254e5300a
Christian Budde Christensen <budde377@gmail.com>
2017-02-28 17:31:44 +0100

    feat: disable publish

--- tools/release.sh
+++ tools/release.sh
@@ -5,9 +5,10 @@
 NPM_VERSION=$(node -p -e "require('./package.json').version")
 export VERSION_CODE=$(git rev-list --count HEAD)
 export VERSION_NAME=${TRAVIS_TAG:-$NPM_VERSION}
-echo "RELEASING ANDROID VERSION: $VERSION_NAME VERSION CODE: $VERSION_CODE"
+echo "Would have beeen RELEASING ANDROID VERSION: $VERSION_NAME VERSION CODE: $VERSION_CODE"
 
 cp secrets/android/my-release-key.keystore android/app/my-release-key.keystore
 cp secrets/android/service-account-credentials.json android/app/service-account-credentials.json
 
-cd android && ./gradlew assembleRelease && ./gradlew publishApkRelease
+cd android && ./gradlew assembleRelease
+# && ./gradlew publishApkRelease
