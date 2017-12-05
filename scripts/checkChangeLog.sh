5814ac680816de0491d6dccb005f1e68544e2a24
Christian Budde Christensen <budde377@gmail.com>
2017-12-05 09:01:51 +0100

    fix: Use right env var

--- scripts/checkChangeLog.sh
+++ scripts/checkChangeLog.sh
@@ -2,7 +2,7 @@
 
 git fetch origin master
 
-if [[ ${TRAVIS_PULL_REQUEST_BRANCH} ]] && ! [[ $TRAVIS_BRANCH == greenkeeper/* ]] && ! [[ $(git diff HEAD..master -- CHANGELOG.md) ]]
+if [[ ${TRAVIS_PULL_REQUEST_BRANCH} ]] && ! [[ $TRAVIS_PULL_REQUEST_BRANCH == greenkeeper/* ]] && ! [[ $(git diff HEAD..master -- CHANGELOG.md) ]]
 then
     echo "On branch $TRAVIS_PULL_REQUEST_BRANCH"
     echo "Remember to change the change log"
